7cecd05f38a3c73ff35280844df6be03c9ea383d
rian <rnevcasey@gmail.com>
2023-05-03 12:12:08 -0400

    Added get posts by id route

--- app.js
+++ app.js
@@ -5,15 +5,73 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.static("public"));
-app.get("/posts", (req, res) => {
+
+app.get("/", (req, res) => {
   const allPosts = postBank.list();
-  const html = `<body>
-  <ul>
-  ${allPosts.map((post) => `<li>${post.name}: ${post.title}</li>`)}
-  </ul>
-  </body>`;
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${allPosts
+        .map(
+          (post) => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>
+            <a href="/posts/${post.id}">${post.title}</a>
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+        )
+        .join("")}
+    </div>
+  </body>
+</html>`;
+
   res.send(html);
 });
+
+app.get("/posts/:id", (req, res) => {
+  const post = postBank.find(req.params.id);
+
+  if (!post.id) {
+    // throw new Error("Not Found");
+    post.title = "Post Not Found";
+    post.content = "";
+    post.name = "";
+  } else {
+    post.name = `(by ${post.name})`;
+  }
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+        <div class='news-item'>
+          <h3>${post.title} <small>${post.name}</small></h3>
+          <p>${post.content}</p>
+        </div>
+    </div>
+  </body>
+</html>`;
+
+  res.send(html);
+});
+
 const PORT = 1337;
 
 app.listen(PORT, () => {
