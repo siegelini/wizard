42e3e401a32c6b445fb6f6017f18f95cf03e8fca
rian <rnevcasey@gmail.com>
2023-05-03 12:22:29 -0400

    Added process.env for port and added dev script

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.get("/posts/:id", (req, res) => {
   res.send(html);
 });
 
-const PORT = 1337;
+const { PORT = 1337 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
